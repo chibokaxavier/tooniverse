001365049a182e43fcb10cb37ed49c922ba8f0a1
manlikexavy <chibokaxavier@gmail.com>
2024-12-14 23:55:42 +0100

    video alternating function complete

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div className='text-orange-400 font-zentry h-dvh'>
-      Awards
-    </div>
+    <main className="relative min-h-screen w-screen overflow-x-hidden ">
+      <Hero />
+    </main>
   );
 }
