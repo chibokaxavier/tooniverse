a1d1cde7bdb7af500d537af91222c5ea2aa841dc
manlikexavy <chibokaxavier@gmail.com>
2024-12-18 11:20:26 +0100

    about section in progress

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 export default function Home() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden ">
       <Hero />
+      <About/>
     </main>
   );
 }
