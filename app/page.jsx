4db5b57f14ff5b015f019fc635149ce427e5af3e
manlikexavy <chibokaxavier@gmail.com>
2024-12-19 17:49:03 +0100

    contact section complete

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Features from "./components/Features";
 import Story from "./components/Story";
+import Contact from "./components/Contact";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <About />
       <Features />
       <Story />
+      <Contact />
     </main>
   );
 }
